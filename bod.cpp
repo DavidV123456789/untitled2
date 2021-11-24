1039bb744a9c98810a44c587d76ffb2a934189f8
map <1david.vdoviak>
2021-11-24 08:45:26 +0100

    j

--- bod.cpp
+++ bod.cpp
@@ -4,59 +4,71 @@
 #include "trojuholnik.h"
 using namespace alpha;
 
+/*
+Bod::Bod() {
+    float x;
+    float y;
+    std::cout<<"Zadaj x: ";
+    std::cin>>x;
+    std::cout<<"Zadaj y: ";
+    std::cin>>y;
+    this->x=x;
+    this->y=y;
+}
+Bod::Bod(float a) {
+    this->x=a;
+    this->y=a;
+}
+Bod::Bod(float a, float b) {
+    this->x=a;
+    this->y=b;
+}
+*/
+/*
 float Bod::getDistance(const Bod &other) const {
 //sqrt((x2-x1)upper2 + (y2-y1)upper2)
-
-    return sqrt(((this->x-other.x)*(this->x-other.x))+((this->y-other.y)*(this->y-other.y)));
+    return sqrt(other.x*other.x+other.y*other.y);
 }
-
 Bod Bod::getCenter(const Bod &other) const {
     Bod N;
     N.x=(this->x+other.x)/2;
     N.y=(this->y+other.y)/2;
     return N;
 }
-
+float Bod::getBod() const {
+    std::cout<<(x,y);
+    return 0;
+}
 Bod Bod::operator+(const Bod &other) const {
     Bod N;
     N.x=this->x+other.x;
     N.y=this->y+other.y;
-
     return N;
 }
-
 Bod Bod::operator/(const Bod &other) const {
     Bod N;
     N.x=this->x+other.x;
     N.y=this->y+other.y;
-
     return N;
 }
-
 Bod operator*(int c, const Bod &other) {
     Bod N;
     N.x=other.x*c;
     N.y=other.y*c;
-
     return N;
 }
-
 Bod operator/(int c, const Bod &other) {
     Bod N;
     N.x=other.x/c;
     N.y=other.y/c;
-
     return N;
 }
-
 Bod Bod::operator*(const Bod &other) const {
     return Bod();
 }
-
 Bod Bod::operator-(const Bod &other) const {
     return Bod();
 }
-
 void Bod::generujPoleBodov(Bod *pole, int n) {
     std::srand(time(0));
     for(int i=0;i<n;i++)
@@ -65,58 +77,173 @@ void Bod::generujPoleBodov(Bod *pole, int n) {
         (pole+i)->y=std::rand()% 99 + 1;
     }
 }
-
 void Bod::utriedPoleBodov(Bod *pole, int n) {
-    qsort((Bod *)pole,n,sizeof(Bod),cmp);
+    qsort((Bod *)pole,n,sizeof(getDistance),cmp);
+}
+*/
+#include <cmath>
+#include <random>
+
+float Bod::getDistance(const Bod &other) const
+{
+    return std::sqrt((x-other.x)*(x-other.x)+(y-other.y)*(y-other.y));
 }
 
-int Bod::cmp(const void *a, const void *b) {
+Bod Bod::getCenter(const Bod &other) const
+{
+    return ((*this)+other)/2.f;
+}
+
+Bod Bod::operator+(const Bod &other) const
+{
+    return {x+other.x,y+other.y};
+}
+
+Bod Bod::operator-(const Bod &other) const
+{
+    return {x-other.x,y-other.y};
+}
+
+Bod Bod::operator/(float k) const
+{
+    return {x/k,y/k};
+}
+
+Bod Bod::operator*(float k) const
+{
+    return {x*k,y*k};
+}
+
+Bod operator*(float k, const Bod &other)
+{
+    return other*k;
+}
+
+std::ostream &operator<<(std::ostream &os, const Bod &other)
+{
+    os<<"[ "<<std::setprecision(4)<<other.x<<","<<other.y<<" ]"<<(float)other<<std::endl;
+    return os;
+}
+
+Bod Bod::operator^(const Bod &other) const
+{
+    return this->getCenter(other);
+}
+
+Bod::operator float() const
+{
+    return this->getDistance(Bod(0,0));
+}
+
+Bod *Bod::generujPoleBodov(int pocetBodov)
+{
+    srand(time(nullptr));
+    Bod *poleBodov;
+    try
+    {
+        poleBodov= new Bod[pocetBodov];
+        for(int i=0;i<pocetBodov;++i)
+        {
+            (poleBodov+i)->x=(float)(rand()%50);
+            (poleBodov+i)->y=(float)(rand()%50);
+        }
+    }
+    catch (const std::bad_alloc & ex)
+    {
+        std::cout<<"Chybna alokacia pamate";
+        return nullptr;
+    }
+    return poleBodov;
+}
+
+void Bod::vypisPoleBodov(int pocetBodov, Bod *poleBodov)
+{
+    for(int i=0;i<pocetBodov;++i)
+    {
+        std::cout<<*(poleBodov+i)<<" ";
+    }
+}
+
+int Bod::cmp(const void *prvy, const void *druhy)
+{
     Bod * A = (Bod *)prvy;
     Bod * B = (Bod *)druhy;
+    //return ((float)(*A)<(float)(*B))?1:((float)(*A)>(float)(*B))?-1:0;
     return ((*A)<(*B))?-1:((*A)>(*B))?1:0;
 }
 
-Bod::operator float() const {
-
-    return 0;
+bool Bod::operator<(const Bod &other)
+{
+    return (float)(*this)<(float)(other);
 }
 
-void Bod::vypisPoleBodov(int pocetBodov, Bod *poleBodov) {
-
+bool Bod::operator>(const Bod &other)
+{
+    return (float)(*this)>(float)(other);
+}
+int Bod::generujInt(int min, int max)
+{
+    std::random_device rd; // ziska trandom cislo z hardveru pocitaca
+    std::mt19937 eng(rd()); // "osoli" generator
+    std::uniform_int_distribution<> distr(min, max); // definuje interval z ktoreho sa generuju cisla
+    return distr(eng); //generuje cislo
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+float Bod::generujFloat(float min, float max)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(min,max);
+    return dis(gen) ;
 }
 
 std::ostream &operator<<(std::ostream &os, const Priamka &other) {
-    os<<"Priamka:  "<<other.X<<" "<<other.Y;
-    return os;
+    return ;
+}
+
+Bod Priamka::getStred() const {//refaktorovanie, pri zmene názvu funkcie zmení názov danej funkcie v celom kóde
+    return X.getCenter(Y);
+    //return X^Y;
+}
+
+float Priamka::getDlzka() const {
+    return X.getDistance(Y);
+}
+
+Vektor Priamka::getsmerovy() const {
+
+    return Y-X;
+}
+
+Vektor Priamka::getNormalovy() const {
+    Vektor smerovy = getsmerovy()
+    return { smerovy.getY()*(-1),smerovy.getX };
+    //getery?
+}
+
+Priamka Priamka::getOsStrany() const {
+    Bod stred = getStred();
+    return {stred, stred+getNormalovy()};
+}
+//Je potrebné optimalozovať program tak aby sa funkcia vyvolávala čo najmenej krát
+PR::PR(Bod A, Bod B) : Priamka(A, B) {
+
+}
+
+PR::PR(const Priamka &P) {
+
+}
+
+void PR::setKoeficienty() {
+
+}
+
+float *PR::getKoeficienty() {
+    return koeficienty;
+}
+
+std::ostream &operator<<(std::ostream &os, const PR &other) {
+    cout<<"Parametricka rovnica:"<<std::endl;
+    cout<<"x= "<<other.koeficienty[0]<<"+"<<koeficienty[2]<<"*t"<<std::endl;
+    cout<<"x= "<<koeficienty[1]<<"+"<<koeficienty[3]<<"*t"<<std::endl;
 }
--- trojuholnik.h
+++ trojuholnik.h
@@ -30,49 +30,71 @@ private:
     float x;
     float y;
 public:
-    Bod():x(0),y(0){}; //vypyta x,y
-    explicit Bod(float a):x(a),y(a){}; //"a" inicializuje x aj y suradnicu
-    Bod(float a, float  b):x(a),y(b){}; // inicializuje x a y
+    Bod():x(0),y(0){};
+    explicit Bod(float a):x(a),y(a){};
+    Bod(float a,float b):x(a),y(b){};
+    float getDistance(const Bod & other = Bod(0,0) ) const;
 
-    float getDistance(const Bod & other = Bod(0, 0)) const; // nech pocita vzdialenostod pociatku [0,0] aj od druheho bodu pouzi impliccitny parameter
-    Bod getCenter(const Bod&other) const;   //najde stred medzi dovma bodmi
+    getX();
+    getY();
 
-    //konstrukcie: vypis bodu [x,y];    sucet bodov;    rozdiel bodov;  bod*cislo;  bod / ciuslo
-    Bod operator+(const Bod &other) const;
-    Bod operator-(const Bod &other) const;
-    Bod operator/(const Bod &other) const;
-    Bod operator*(const Bod &other) const;
-    friend Bod operator*(int c, const Bod &other);
-    friend Bod operator/(int c, const Bod &other);
-    explicit operator float() const; //pretypuje na float [x,y]
+    Bod getCenter(const Bod & other) const;
+    Bod operator+(const Bod & other) const;
+    Bod operator-(const Bod & other) const;
+    Bod operator/(float k) const;
+    Bod operator*(float k) const;
+    bool operator<(const Bod & other);
+    bool operator>(const Bod & other);
+    Bod operator^(const Bod & other) const; //pretazeny operator strieska na vypocet stredu usecky
+    explicit operator float() const;  //pretazeny operator konverzie na float, ktory vrati vzdialenost od [0,0]
+    //inicializacia generatorov cisel
+    friend Bod operator*(float k, const Bod &other);
+    friend std::ostream & operator<<(std::ostream & os,const Bod &other);
+    //staticka metoda ktora vygeneruje pole bodov zadaneho poctu a utriedi ho podla vzdialenosti bodu od pociatku
 
-        //staticka metoda ktora vygeneruje pole n bodov, ktoré potom utrriedi podla vzdialenosti bodu od pociatku
-   static void generujPoleBodov(Bod *pole, int n);
-   //musis dynamicky alokovat pole
-   static void utriedPoleBodov(Bod *pole, int n);
-   static void vypisPoleBodov(int pocetBodov, Bod *poleBodov);
-   static int cmp(const void * a, const void * b);
+    static Bod * generujPoleBodov(int pocetBodov);
+    static void vypisPoleBodov(int pocetBodov,Bod *poleBodov);
+    static int cmp(const void *prvy,const void *druhy);
+    static int generujInt(int min, int max);
+    static float generujFloat(float min, float max);
 };
+//alias
+using Vektor = Bod; //aby sme mohli pouzivat vektor namiesto vtedy, ked to je vhodne a bolo vidiet presne co robime
+
 
 class Priamka{
-private:
+protected:
     Bod X;
-    Bod Y;
+    Bod Y;//aby jej dedicne triedy mohli pouzivat tuto vlastnost
 public:
     //
     Priamka(){};//Priamka():X({0 , 0}),Y({0,0}){};-funguje bez toho
     explicit Priamka(Bod Z):X(Z),Y(Z){};
     Priamka(Bod Z, Bod W):X(Z),Y(W){};
     friend std::ostream &operator<<(std::ostream &os, const Priamka & other);
-
-
-
-
+    Bod getStred() const;
+    float  getDlzka() const;
+    Vektor getsmerovy() const;
+    Vektor getNormalovy() const;//kolmý vektor na smerový
+    Priamka getOsStrany() const;
 
 
 
 };
 
+class PR: public Priamka{
+private:
+    float koeficienty[4];//koeficienty a1,s1,a2,s2 v rovniciach: x = a1+s1*t;   y=a2+s2*t;
+public:
+    PR():koeficienty{0,0,0,0}{};//ak nezadame nic tak vsetky koeficienty budu 0
+    PR(Bod A, Bod B);//parametrivka priamka urcena dvoma bodmi
+    explicit PR(const Priamka & P);//vyrobi parametricku rovnicu z uz vytvorenej priamky
+    float &operator [](int index) {return  koeficienty[index];};//musime vratit odkaz na float// pretazeny opertor [] pre nekonstantny pristup
+    const float & operator [](int index) const{return koeficienty[index];};// pretazeny opertor [] pre konstantny pristup
+    friend  std::ostream  & operator<<(std::ostream & os, const PR & other);
+    float * getKoeficienty();//vrati pole koeficientov
+    void setKoeficienty();//nastavi koeficienty v parametrckej rovnici na spravne hodnoty
 
+};
 
 #endif //UNTITLED2_TROJUHOLNIK_H
